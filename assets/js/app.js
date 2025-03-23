6228b29cd2df79bdbb8e9f672900bf64c474ab1c
pierre <pierre.couderc06@gmail.com>
2025-03-23 04:24:25 +0100

    ajout menu jeu + battle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,56 @@
+/* JEU */
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    menu.classList.remove("hideMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
+
+    const mainMenuPlay = document.getElementById("button_main-menu-play");
+    const mainMenuSettings = document.getElementById("button_main-menu-setting");
+    const alternance = document.getElementById("with-grid_play-menu");
+    const alternance2 = document.getElementById("with-grid_play-battle");
+    const exit = document.getElementById("exit");
+
+
+    mainMenuPlay.addEventListener("click", (event) => {
+        console.log("click marche");
+        alternance.style.display = "none";
+        alternance2.style.display = "grid";
+
+});
+exit.addEventListener("click", (event) => {
+    console.log("click marche");
+    alternance.style.display = "grid";
+    alternance2.style.display = "none";
+
+});
+
+
+    
+/* TEST */
+
+
 let notes = [4, 9, 7, 8, 12, 2];
 notes.sort();
 console.log(notes);
