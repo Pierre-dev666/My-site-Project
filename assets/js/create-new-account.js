fc01b89b87d2320228464ef1aceea5d0126ddde4
pierre <pierre.couderc06@gmail.com>
2025-03-25 19:16:58 +0100

    dépot et récupération local storage

--- assets/js/create-new-account.js
+++ assets/js/create-new-account.js
@@ -1,14 +1,19 @@
 /* créer un compte */
 const createNewAccountButton = document.getElementById("create-new-account_submit");
 const idText = document.getElementById("create-username");
+const passwordText = document.getElementById("create-password");
 
 createNewAccountButton.onclick = function () {
-  let text = idText.value;
-  if (text === "pierre"){
-    console.log("créer ça marche")
-  }
-  else{
-    console.log("error")
-  }
-  
+    let identidiant = idText.value;
+    let pass = passwordText.value;
+    if (identidiant.length >= 7 && pass.length >= 7) {
+        localStorage.setItem("myIdentifiant", identidiant);
+        localStorage.setItem("myPassword", pass);
+
+    }
+    else {
+        console.log("error")
+        localStorage.clear();
+    }
+
 }
--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -1,10 +1,15 @@
 /* Se connecter */
 const logInButton = document.getElementById("log-in_submit");
 const idLogInText = document.getElementById("username");
+const passLogInText = document.getElementById("pass");
 
 logInButton.onclick = function () {
   let logInText = idLogInText.value;
-  if (logInText === "pierre"){
+  let passText = idLogInText.value;
+  const identifiant = localStorage.getItem("myIdentifiant");
+  const pass = localStorage.getItem("myPassword");
+
+  if (logInText === identifiant && passText === pass){
     console.log("ça marche")
   }
   else{
